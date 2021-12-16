8e9aa510b6fd75278f8448007918a899820e880f
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-16 09:30:46 +0900

    戻るボタンを押した際にページがちゃんと移動するように

--- app/src/main/java/com/example/amicaforandroid/MainActivity.kt
+++ app/src/main/java/com/example/amicaforandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.amicaforandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
@@ -15,5 +16,15 @@ class MainActivity : AppCompatActivity() {
         webView.settings.javaScriptEnabled = true
         webView.setWebViewClient(WebViewClient())
         webView.loadUrl("https://cherrypie-system.herokuapp.com/")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        val webView = findViewById<WebView>(R.id.webView)
+        // 端末の戻るボタンでブラウザバック
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
