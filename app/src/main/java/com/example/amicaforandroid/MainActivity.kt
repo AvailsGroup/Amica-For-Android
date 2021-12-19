01ccc4389256600edb63c0a680649917775d9d88
yurisi0212 <Dev.yurisi@gmail.com>
2021-12-19 10:30:44 +0900

    アプリケーションバーの除去、アイコンの変更、ローディング画面の追加

--- app/src/main/java/com/example/amicaforandroid/MainActivity.kt
+++ app/src/main/java/com/example/amicaforandroid/MainActivity.kt
@@ -3,24 +3,43 @@ package com.example.amicaforandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import android.graphics.Bitmap
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val webView = findViewById<WebView>(R.id.webView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar2);
+
         webView.settings.domStorageEnabled = true
         webView.settings.javaScriptEnabled = true
-        webView.setWebViewClient(WebViewClient())
+
+        webView.webViewClient = object: WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progressBar.visibility = ProgressBar.VISIBLE
+                super.onPageStarted(view, url, favicon)
+            }
+            override fun onPageFinished(view: WebView?, url: String?) {
+                progressBar.visibility = ProgressBar.INVISIBLE
+                super.onPageFinished(view, url)
+            }
+        }
         webView.loadUrl("https://cherrypie-system.herokuapp.com/")
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         val webView = findViewById<WebView>(R.id.webView)
-        // 端末の戻るボタンでブラウザバック
         if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
             webView.goBack()
             return true
